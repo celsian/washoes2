775e4b472c91a42eb3d3f380eeb44cebbd7a2a93
Celsian <celsian@gmail.com>
2013-08-13 19:37:28 -0700

    Heroku Config update for CSS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ Washoes2::Application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
